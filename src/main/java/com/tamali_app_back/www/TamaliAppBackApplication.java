d803ff5bb9d2c7bc39c1814f6ea8b5c7e74f2461
Amadou Landoure <amadoulandoure004@gmail.com>
2026-02-08 12:25:30 +0000

    first commit

---
+++ src/main/java/com/tamali_app_back/www/TamaliAppBackApplication.java
@@ -0,0 +1,13 @@
+package com.tamali_app_back.www;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamaliAppBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamaliAppBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamali_app_back/www/TamaliAppBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamali_app_back.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TamaliAppBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
